88df3e080956742c152258b9d0428d3bf357cafc
Masayoshi Wada <developer@andantesoftware.com>
2015-06-26 02:22:47 +0900

    initialize config file automatically

--- lib/owlproxy/settings.rb
+++ lib/owlproxy/settings.rb
@@ -1,10 +1,18 @@
 require "settingslogic"
+require "fileutils"
 
-CONFIG_PATH=".owlproxy.yml"
+CONFIG_FILE=".owlproxy.yml"
+CONFIG_PATH="#{Dir.home}/#{CONFIG_FILE}"
 
 module OwlProxy
+  # init config file
+  src = "#{File.dirname(__FILE__)}/config/owlproxy.yml.example"
+  dst = CONFIG_PATH
+  FileUtils.cp src, dst unless File.exist? dst
+
   class Settings < Settingslogic
-    source "#{Dir.home}/#{CONFIG_PATH}"
+    source CONFIG_PATH
     load!
   end
 end
+
